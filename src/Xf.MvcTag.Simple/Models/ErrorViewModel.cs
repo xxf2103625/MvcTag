2eed16702782a7c763e85ac7ef7905466ec63f7f
xxf <2103625@qq.com>
2018-12-13 03:46:41 +0800

    工程项目初始化

---
+++ src/Xf.MvcTag.Simple/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Xf.MvcTag.Simple.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
